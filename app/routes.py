68d9aff8e46d2a40f2c6d9152c7faf197c91e3ca
boogerman919 <raytsai919@gmail.com>
2022-01-16 16:58:56 -0800

    able to create ride from frontend

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,18 @@
 from app import app, db
 from app.models import Ride, User
+from flask import request
+from datetime import datetime
 
 @app.route('/')
-@app.route('/starttime/<user_id>', methods=['GET'])
-def index(user_id):
-    # get the last ride of the user from the database
-    r = User.query.get(user_id).rides[-1]
-    return {"id": r.id, "start_time": str(r.start_time)}
+
+@app.route('/start_ride', methods=['POST'])
+def start_ride():
+    user_id = request.json['user_id']
+    board_id = request.json['board_id']
+
+    ride = Ride(user_id=user_id, board_id=board_id)
+
+    db.session.add(ride)
+    db.session.commit()
+
+    return {"ride_id": ride.id, "start_time": ride.start_time.strftime("%Y-%m-%dT%H:%M:%SZ")}
